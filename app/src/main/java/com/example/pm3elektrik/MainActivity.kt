d3040f5045b9eb5cb1475321bde5161047be115c
Huseyin <h.ozsoy.tr88@gmail.com>
2019-07-20 13:51:48 +0300

    initial commit

---
+++ app/src/main/java/com/example/pm3elektrik/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pm3elektrik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
