6d74adbeeb00b2f373ad6d27d9af3fa5d9d7ebdf
Huseyin <h.ozsoy.tr88@gmail.com>
2019-08-06 10:46:37 +0300

    initial commit

---
+++ app/src/main/java/com/example/pm3elektrik/DigerBilgilerSayfasi/AmbarKayitSayfasi/AmbarKayit.kt
@@ -0,0 +1,24 @@
+package com.example.pm3elektrik.DigerBilgilerSayfasi.AmbarKayitSayfasi
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.pm3elektrik.R
+
+
+class AmbarKayit : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_ambar_kayit, container, false)
+
+
+
+        return view
+    }
+
+
+}
