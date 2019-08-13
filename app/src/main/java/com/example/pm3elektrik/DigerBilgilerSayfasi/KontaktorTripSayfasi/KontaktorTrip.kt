f5d844540b01fcb65021f884fa1d006dd3d771af
Huseyin <h.ozsoy.tr88@gmail.com>
2019-08-13 13:12:56 +0300

    initial commit

--- app/src/main/java/com/example/pm3elektrik/DigerBilgilerSayfasi/KontaktorTripSayfasi/KontaktorTrip.kt
+++ app/src/main/java/com/example/pm3elektrik/DigerBilgilerSayfasi/KontaktorTripSayfasi/KontaktorTrip.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.FragmentTransaction
+import com.example.pm3elektrik.DigerBilgilerSayfasi.KontaktorTripSayfasi.KontaktorTripSiniflari.SizeLowRange
 
 import com.example.pm3elektrik.R
 
@@ -14,9 +17,37 @@ class KontaktorTrip : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_kontaktor_trip, container, false)
 
+        val sizeLowRange = view.findViewById<CardView>(R.id.cardViewLowRange)
+        val sizeBir = view.findViewById<CardView>(R.id.cardViewSize1)
+        val sizeIki = view.findViewById<CardView>(R.id.cardViewSize2)
+        val sizeUc = view.findViewById<CardView>(R.id.cardViewSize3)
+        val sizeDort = view.findViewById<CardView>(R.id.cardViewSize4)
+        val sizeBes = view.findViewById<CardView>(R.id.cardViewSize5)
+
+        sizeLowRange.setOnClickListener {
+            changeFragment(SizeLowRange())
+        }
+
+        sizeBir.setOnClickListener {  }
+        sizeIki.setOnClickListener {  }
+        sizeUc.setOnClickListener {  }
+        sizeDort.setOnClickListener {  }
+        sizeBes.setOnClickListener {  }
+
+
+
 
 
         return view
+    }
+
+
+    private fun changeFragment(fragment : Fragment){
+
+        val fragmentTransaction : FragmentTransaction = activity?.supportFragmentManager!!.beginTransaction()
+        fragmentTransaction.replace(R.id.containerKontaktorTripSayfasi,fragment,"kontaktor_trip_sayfasi")
+        fragmentTransaction.commit()
+
     }
 
 
---
+++ app/src/main/java/com/example/pm3elektrik/DigerBilgilerSayfasi/KontaktorTripSayfasi/KontaktorTripSiniflari/SizeLowRange.kt
@@ -0,0 +1,24 @@
+package com.example.pm3elektrik.DigerBilgilerSayfasi.KontaktorTripSayfasi.KontaktorTripSiniflari
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.pm3elektrik.R
+
+class SizeLowRange : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_size_low_range, container, false)
+
+
+
+
+        return view
+    }
+
+
+}
