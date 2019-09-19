ba446d8650a0761514b503af0fc18775224e423a
Huseyin <h.ozsoy.tr88@gmail.com>
2019-09-19 05:57:39 +0300

    initial commit

--- app/src/main/java/com/example/pm3elektrik/DigerBilgilerSayfasi/TrafoSayfasi/TrafoDuzenleDegistirDialogFR/TrafoDuzenleDegistirDialog.kt
+++ app/src/main/java/com/example/pm3elektrik/DigerBilgilerSayfasi/TrafoSayfasi/TrafoDuzenleDegistirDialogFR/TrafoDuzenleDegistirDialog.kt
@@ -2,7 +2,9 @@ package com.example.pm3elektrik.DigerBilgilerSayfasi.TrafoSayfasi.TrafoDuzenleDe
 
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,6 +14,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.pm3elektrik.DigerBilgilerSayfasi.TrafoSayfasi.TrafoEtiketDuzenle.TrafoGosterDuzenle
 
 import com.example.pm3elektrik.R
 
@@ -27,20 +31,13 @@ class TrafoDuzenleDegistirDialog : DialogFragment() {
 
         fotoYukle.setOnClickListener {
 
-            val intent = Intent(Intent.ACTION_GET_CONTENT)
-            intent.type = "image/*"
-            startActivityForResult(intent,100)
+            ekle.setOnClickListener {
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                intent.type = "image/*"
+                startActivityForResult(intent, 100)
+
+            }
         }
-
-        ekle.setOnClickListener {
-
-
-
-
-
-        }
-
-
 
         return view
     }
@@ -51,10 +48,8 @@ class TrafoDuzenleDegistirDialog : DialogFragment() {
         if(requestCode == 100 && resultCode == Activity.RESULT_OK && data != null){
 
             val galeridenGelenResimYolu = data.data
-            Log.e("GelenResim","$galeridenGelenResimYolu")
 
+            TrafoGosterDuzenle().gelenResimUri(galeridenGelenResimYolu)
         }
     }
-
-
 }
--- app/src/main/java/com/example/pm3elektrik/DigerBilgilerSayfasi/TrafoSayfasi/TrafoEtiketDuzenle/TrafoGosterDuzenle.kt
+++ app/src/main/java/com/example/pm3elektrik/DigerBilgilerSayfasi/TrafoSayfasi/TrafoEtiketDuzenle/TrafoGosterDuzenle.kt
@@ -1,8 +1,7 @@
 package com.example.pm3elektrik.DigerBilgilerSayfasi.TrafoSayfasi.TrafoEtiketDuzenle
 
-
-import android.content.Context
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -11,22 +10,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.pm3elektrik.DigerBilgilerSayfasi.TrafoSayfasi.TrafoDuzenleDegistirDialogFR.TrafoDuzenleDegistirDialog
 
 import com.example.pm3elektrik.R
+import com.github.chrisbanes.photoview.PhotoView
+import com.squareup.picasso.Picasso
 
-class TrafoGosterDuzenle : Fragment() {
+class TrafoGosterDuzenle : Fragment(){
 
     var trafoIsim: String? = null
     var izinlerVerildi = false
+    var gelenResimUri : Uri? = null
+    var photoView : PhotoView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_trafo_goster_duzenle, container, false)
 
         val edit = view.findViewById<ImageView>(R.id.imgTrafoEtiketDuzenle)
         val textIsim = view.findViewById<TextView>(R.id.tvTrafoEtiketIsim)
+
 
         val bundle: Bundle? = arguments
         trafoIsim = bundle?.getString("rvGelenTrafoIsim")
@@ -59,7 +64,7 @@ class TrafoGosterDuzenle : Fragment() {
 
             izinlerVerildi = true
         }else {
-            ActivityCompat.requestPermissions(activity!!.parent,izinler,150)
+            ActivityCompat.requestPermissions(activity!!,izinler,150)
         }
     }
 
@@ -71,7 +76,23 @@ class TrafoGosterDuzenle : Fragment() {
                 val dialogFragment = TrafoDuzenleDegistirDialog()
                 dialogFragment.show(fragmentManager!!,"trafo_duzenle_dialog_fr")
             }
+        }else {
+
+            Toast.makeText(view?.context,"Tüm İzinleri Vermeniz Gerekiyor",Toast.LENGTH_LONG).show()
         }
+
+    }
+
+    fun gelenResimUri(resim: Uri?) {
+
+        gelenResimUri = resim
+
+        photoView = view?.findViewById<PhotoView>(R.id.imageView4)
+        Log.e("gelenresim","$resim")
+
+        Picasso.get().load(resim).into(photoView)
+
+
 
     }
 
